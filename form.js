20440990ce76393a7c7e9996dfafca127bfc272b
MrrSamoeurnjr <133938508+MrrSamoeurnjr@users.noreply.github.com>
2023-06-23 08:32:08 -0700

    Add files via upload

---
+++ form.js
@@ -0,0 +1,125 @@
+let username = document.getElementById("username");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let form = document.getElementById("form");
+// let demo = document.getElementById("demo");
+function help(){
+    var body = document.getElementsByTagName('body')[0];
+    if( username.value == 'kaka' || email.value == 'samoeurn6@gmail.com' || password.value == '123457' ) {
+            body.style.backgroundImage = 'url(./img1.jpg)';
+            var audio = new Audio('Frog Laughing - Sound Effect_256k.mp3');
+audio.play();
+    }
+    else if(username.value == 'Bona' || email.value == 'rithbona7@gmail.com' || password.value == '778878'){
+        body.style.backgroundImage = 'url(./img11.jpg)';
+        var audio = new Audio('Frog Laughing - Sound Effect_256k.mp3');
+audio.play();
+    }
+    else if(username.value == 'ChannyvLoveDaro' || email.value == 'savathchanny8@gmail.com' || password.value == '887748'){
+        body.style.backgroundImage ='url(./img12.jpg)';
+        var audio = new Audio ('Frog Laughing - Sound Effect_256k.mp3');
+audio.play();
+    }
+    else if (username.value == 'Sreynang' || email.value == 'rathsreynang8@gmail.com' || password.value == '736389'){
+        body.style.backgroundImage = 'url(./img13.jpg)';
+        var audio = new Audio('Frog Laughing - Sound Effect_256k.mp3');
+audio.play();
+
+
+    }
+    else if(username.value == 'Sievmeycutecute' || email.value == 'solsievmey9@gmail.com' || password.value == '876544'){
+        body.style.backgroundImage = 'url(./img14.jpg)';
+        var audio = new Audio('Frog Laughing - Sound Effect_256k.mp3');
+audio.play();
+    }
+    else if(username.value == 'KhimloveVanetbigbig' || email.value == 'nakkhim10@gmail.com' || password.value == '766543'){
+        body.style.backgroundImage = 'url(./img15.jpg)';
+        var audio = new Audio('Frog Laughing - Sound Effect_256k.mp3');
+audio.play();
+    }
+    else if(username.value == 'Khunary' || email.value == 'artkhunary11@gmail.com' || password.value == '765446'){
+        body.style.backgroundImage = 'url(./img16.jpg)';
+        var audio = new Audio('Frog Laughing - Sound Effect_256k.mp3');
+audio.play();
+    }
+    else if(username.value == 'KaTouchJM' || email.value == 'noeurnsreytouch12@gmailc.om' || password.value == '242343'){
+        body.style.backgroundImage = 'url(./img17.jpg)';
+        var audio = new Audio('Frog Laughing - Sound Effect_256k.mp3');
+audio.play();
+    }
+    else if(username.value == 'lizamapmap' || email.value == 'rithyly13@gmail.com' || password.value == '655439'){
+        body.style.backgroundImage = 'url(./img13.jpg)';
+        var audio = new Audio('Frog Laughing - Sound Effect_256k.mp3');
+audio.play();
+    }
+    else if(username.value == 'Sinablackeyesandmap' || email.value == 'remsina14@gmail.com' || password.value == '654324'){
+        body.style.backgroundImage = 'url(./img12.jpg)';
+        var audio = new Audio('Frog Laughing - Sound Effect_256k.mp3');
+audio.play();
+    }
+    else if(username.value == 'Houysiscute' || email.value == 'sushouy14@gmail.com' || password.value == '654325'){
+        body.style.backgroundImage = 'url(./img14.jpg)';
+        var audio = new Audio('Frog Laughing - Sound Effect_256k.mp3');
+audio.play();
+    }
+    else if(username.value == 'Sachak' || email.value == 'chemsachack15@gmail.com' || password.value == '654336'){
+        body.style.backgroundImage = 'url(./img2.jpg)';
+        var audio = new Audio('Frog Laughing - Sound Effect_256k.mp3');
+audio.play();
+    }
+    else if(username.value == 'Bunrong' || email.value == 'thoungbunrong' || password.value == '773993'){
+        body.style.backgroundImage = 'url(./img3.jpg)';
+        var audio = new Audio('Frog Laughing - Sound Effect_256k.mp3');
+audio.play();
+    }
+    else if(username.value == 'Chentha' || email.value == 'nymchentha5@gmail.com' || password.value == '665544'){
+        body.style.backgroundImage = 'url(./img4.jpg)';
+        var audio = new Audio('Frog Laughing - Sound Effect_256k.mp3');
+audio.play();
+    }
+    else if(username.value == 'Udom' || email.value == 'dengudom8@gmail.com' || password.value == '887766'){
+        body.style.backgroundImage = 'url(./img5.jpg)';
+        var audio = new Audio('Frog Laughing - Sound Effect_256k.mp3');
+audio.play();
+    }
+    else if(username.value == 'Sambath' || email.value == 'heinratanaksambath@gmail.com' || password.value == '766544'){
+        body.style.backgroundImage = 'url(./img6.jpg)';
+        var audio = new Audio('Frog Laughing - Sound Effect_256k.mp3');
+audio.play();
+    }
+    else if(username.value == 'Vanet' || email.value == 'neangvanet6@gmail.com' || password.value == '766544'){
+        body.style.backgroundImage = 'url(./img7.jpg)';
+        var audio = new Audio('Frog Laughing - Sound Effect_256k.mp3');
+audio.play();
+    }
+    else if(username.value == 'Samphas' || email.value == 'rithsamphas7@gmail.com' || password.value == '766544'){
+        body.style.backgroundImage = 'url(./img8.jpg)';
+        var audio = new Audio('Frog Laughing - Sound Effect_256k.mp3');
+audio.play();
+    }
+    else if(username.value == 'Perith' || email.value == 'phyperith6@gmail.com' || password.value == '765433'){
+        body.style.backgroundImage = 'url(./img9.jpg)';
+        var audio = new Audio('Frog Laughing - Sound Effect_256k.mp3');
+audio.play();
+    }
+    else if(username.value == 'Panha' || email.value == 'nonpanha4@gmail.com' || password.value == '776657'){
+        body.style.backgroundImage = 'url(./img10.jpg)';
+        var audio = new Audio('Frog Laughing - Sound Effect_256k.mp3');
+audio.play();
+    }
+    else {
+        alert("Please try again!")
+    }
+}
+
+// let Email = document.getElementById("email2");
+// // console.log("email2")
+// let Password = document.getElementById("password2");
+// let demo = document.getElementById("demo");
+// function displayInfo(){
+
+//     if(Email.value == 'samoeurnart6@gmail.com' || Password.value == '882003'){
+//         demo.innerHTML += '<h2>Success</h2>';
+//         // alert("Hello everyone");
+//     }
+// }
